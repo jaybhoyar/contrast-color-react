c96f39ccd5f25046fbb72f1ca72d806da8a8bdc8
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-12 13:02:00 +0530

    invert color

--- src/App.js
+++ src/App.js
@@ -2,39 +2,97 @@ import React, { Component } from "react";
 import randomColorPair from "./randomColor.js";
 import "./App.scss";
 import Header from "./Header";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pColor: randomColorPair()
-    };
-  }
-  render() {
-    return (
-      <>
-        <Header {...this.state.pColor} />
-        <div
-          style={{ backgroundColor: this.state.pColor[0] }}
-          className="container"
-        >
-          <h4 style={{ borderColor: this.state.pColor[1], color: this.state.pColor[1] }}>{this.state.pColor[2]}  Contrast {this.state.pColor[3]}</h4>
-          <p style={{ color: this.state.pColor[1] }}>
-            Lorem Ipsum is simply dummy text of the printing and
+	constructor(props) {
+		super(props);
+		this.state = {
+			pColor: randomColorPair().pair,
+			ratio: randomColorPair().ratio,
+			score: randomColorPair().score
+		};
+	}
+	changeColor() {
+		this.setState({
+			pColor: randomColorPair().pair,
+			ratio: randomColorPair().ratio,
+			score: randomColorPair().score
+		});
+		console.log(this.pColor);
+	}
+	invertColor() {
+		this.setState({ pColor: this.state.pColor.reverse() });
+	}
+	clickToCopy() {
+		var range = document.createRange();
+		// range.selectNode(event.target);
+		window.getSelection().removeAllRanges(); // clear current selection
+		window.getSelection().addRange(range); // to select text
+		document.execCommand("copy");
+	}
+	render() {
+		return (
+			<>
+				<Header {...this.state.pColor} />
+				<div
+					style={{ backgroundColor: this.state.pColor[0] }}
+					className="container"
+				>
+					<div className="toggle_colors">
+						<button
+							style={{
+								color: this.state.pColor[0],
+								backgroundColor: this.state.pColor[1]
+							}}
+							className="color_code"
+						>
+							{this.state.pColor[1]}
+						</button>
+						<button
+							className="next_color"
+							style={{
+								color: this.state.pColor[0],
+								backgroundColor: this.state.pColor[1]
+							}}
+							onClick={() => this.changeColor()}
+						>
+							Next ↻
+						</button>
+						<span style={{ color: this.state.pColor[1] }}>
+							{this.state.ratio} Contrast {this.state.score}
+						</span>
+						<button
+							className="next_color"
+							style={{
+								color: this.state.pColor[0],
+								backgroundColor: this.state.pColor[1]
+							}}
+							onClick={() => this.invertColor()}
+						>
+							Invert ⇋
+						</button>
+						<button
+							style={{
+								color: this.state.pColor[0],
+								backgroundColor: this.state.pColor[1]
+							}}
+							className="color_code"
+						>
+							{this.state.pColor[0]}
+						</button>
+					</div>
+
+					<p style={{ color: this.state.pColor[1] }}>
+						Lorem Ipsum is simply dummy text of the printing and
 						typesetting industry. Lorem Ipsum has been the
 						industry's standard dummy text ever since the 1500s,
 						when an unknown printer took a galley of type and
-						scrambled it to make a type specimen book. It has
-						survived not only five centuries, but also the leap into
-						electronic typesetting, remaining essentially unchanged.
-						It was popularised in the 1960s with the release of
-						Letraset sheets containing Lorem Ipsum passages, and
-						more recently with desktop publishing software like
-						Aldus PageMaker including versions of Lorem Ipsum.
+						scrambled it to make a type specimen book.
 					</p>
-        </div>
-      </>
-    );
-  }
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
