f1363b26914be63e86727d30a0fa2d2ce21cc768
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-13 20:29:04 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -77,6 +77,9 @@ class App extends Component {
 								backgroundColor: this.state.pColor[1]
 							}}
 							className="color_code"
+							onClick={() =>
+								this.clickToCopy(this.state.pColor[1])
+							}
 						>
 							{this.state.pColor[0]}
 						</button>
